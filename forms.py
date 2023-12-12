cba74f6ea049046d4ba111e57bf789a80c1aad93
Justin Kawaja <jkawaja909@gmail.com>
2023-12-12 19:43:23 -0330

    updated forms file

--- forms.py
+++ forms.py
@@ -1,14 +1,21 @@
+from decimal import Decimal
+import decimal
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileRequired, FileField
-from wtforms.fields import StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import SubmitField
+from wtforms.fields import SelectField, IntegerField, DecimalField
+from wtforms.validators import DataRequired, NumberRange
 
-class RecipeForm(FlaskForm):
-    recipe_name = StringField('Recipe Name:', validators=[DataRequired()])
-    recipe_ingredients = StringField('Recipe Ingredients:', validators=[DataRequired()])
-    recipe_prep_instructions = TextAreaField('Preparation Instructions:', validators=[DataRequired()])
-    recipe_serving_instructions = TextAreaField('Serving Instructions:', validators=[DataRequired()])
-    recipe_picture = FileField('Recipe Picture:', validators=[FileRequired()])
+class CarInfoForm(FlaskForm):
+    carCylinders = IntegerField("Cylinders:", default=3, validators=[DataRequired(), NumberRange(min=3, max=8)],
+                                render_kw={"title": "Cylinders must be between 3 and 8."})
+    carHorsepower = DecimalField("Horsepower:", default=Decimal(40), places=2, rounding=decimal.ROUND_UP,
+                                 validators=[DataRequired(), NumberRange(min=40, max=250)],
+                                 render_kw={"title": "Horsepower must be between 40 and 250."})
+    carWeight = IntegerField("Weight:", default=1600, validators=[DataRequired(), NumberRange(min=1600, max=5500)],
+                             render_kw={"title": "Weight must be between 1600 and 5500."})
+    carYear = IntegerField("Year:", default=1965, validators=[DataRequired(), NumberRange(min=1965, max=1987)],
+                           render_kw={"title": "Year must be between 1965 and 1987."})
+    carOrigin = SelectField("Origin:", choices=[("USA", "USA"), ("Japan", "Japan"),  ("Europe", "Europe")],
+                            validators=[DataRequired()])
 
-class SearchForm(FlaskForm):
-    search_name = StringField('', validators=[DataRequired()])
+    submit = SubmitField("Submit")
